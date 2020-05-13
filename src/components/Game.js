c386568b657ec83e3bcedc6f1d218ec30fcc25d2
paszkevi4 <paszkevi4@aol.com>
2020-05-13 13:46:43 +0300

    immutable state for game history

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,22 +5,34 @@ import Board from './Board'
 
 const Game = () => {
 
-    let [board, setBoard] = useState(Array(9).fill(null))
-    let [xIsNext, setXIsNext] = useState(false)
-    let winner = calculateWinner(board)
+    let [history, setHistory] = useState([Array(9).fill(null)])
+    let [step, setStep] = useState(0)
+    let [xIsNext, setXIsNext] = useState(true)
+    const winner = calculateWinner(history[step])
 
     const handleClick = (i) => {
-        let newBoard = [...board]
-        if (winner || newBoard[i]) return
-        newBoard[i] = xIsNext ? 'X' : 'O'
-        setBoard(newBoard)
+        const newHistory = [...history[step]]
+        if (winner || newHistory[i]) return
+        newHistory[i] = xIsNext ? 'X' : 'O'
+        setHistory([...history, newHistory])
+        setStep(history.length)
         setXIsNext(!xIsNext)
-
     }
 
 
+    const restartGame = () => (
+        <button className={restartGame} onClick={ () => setHistory([Array(9).fill(null)]) }>
+            restart
+        </button>
+    )
+
     return (
-        <Board squares={board} onClick={handleClick}/>
+        <div>
+            <Board squares={history[step]} onClick={handleClick}/>
+            <p>{winner ? 'The winner is ' + winner : 'Next player is: ' + (xIsNext ? 'X' : 'O' )}</p>
+            {restartGame}
+            <button onClick={() => console.log(history)}></button>
+        </div>
     )
 }
 
