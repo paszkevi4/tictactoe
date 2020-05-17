781b028ea8b0922c8a1a036c8f5c7055a3933b36
paszkevi4 <paszkevi4@aol.com>
2020-05-17 18:44:51 +0300

    Updates

---
+++ precache-manifest.8d11f001baaec0644d96e47b3890b019.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8145a9269dc0db2bbe0004312a737ffd",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "75e3f1a3e7d4faf568d4",
+    "url": "/tictactoe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3cd07ca39d98269c1c51",
+    "url": "/tictactoe/static/css/main.b1b9738b.chunk.css"
+  },
+  {
+    "revision": "75e3f1a3e7d4faf568d4",
+    "url": "/tictactoe/static/js/2.d1acb3c1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tictactoe/static/js/2.d1acb3c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd07ca39d98269c1c51",
+    "url": "/tictactoe/static/js/main.9a14667d.chunk.js"
+  },
+  {
+    "revision": "d6a76b3547955ded53da",
+    "url": "/tictactoe/static/js/runtime-main.986fe205.js"
+  }
+]);
